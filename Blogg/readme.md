38d091348758cb3b9250ca775494992b68d88719
Syver Storm-Furru <syver.storm.furru@gmail.com>
2018-03-03 15:49:11 +0100

    La til enkel blogg, med en del skarpe kanter.

---
+++ Blogg/readme.md
@@ -0,0 +1,9 @@
+## Blogg
+
+Bloggen kan skrive nye innlegg, og legge til kommentarer på det nyeste innlegget.
+ 
+TODO:
+-  Ser ikke pen ut, trenger bedre CSS.
+-  Titler på innlegg.
+
+
---
+++ Blogg/skript.js
@@ -0,0 +1,50 @@
+var innlegg = [];
+
+
+function oppdaterInnlegg() {
+    var innleggBoks = document.getElementById("innlegg-boks");
+    var nyHTML = "";
+    for(var i = 0; i < innlegg.length; i++) {
+	var detteInnlegget = innlegg[i];
+	nyHTML += "<div class=\"innlegg\">"
+	nyHTML += "<div class=\"tittel\">Innlegg nummer " + i + "</div>";
+	nyHTML += "<div class=\"tekst\">" + detteInnlegget.tekst + "</div>";
+	nyHTML += "<div id=\"kommentar\"><div class=\"tidligere\">";
+	for(var j = 0; j < detteInnlegget.kommentarer.length; j++) {
+	    nyHTML += "<h3>" + detteInnlegget.kommentarer[i].navn + "</h3>";
+	    nyHTML += "<p>" + detteInnlegget.kommentarer[i].kommentar + "</p>";
+	}
+
+	nyHTML += "</div><p>Navn: <input type=\"text\" id=\"kommentar-navn\"></input></p><p>Kommentar: <input type=\"text\" id=\"kommentar-kommentar\"></input></p><p><button id=\"kommentar-send\">Legg til kommentar</button></p></div></div>";
+	console.log(JSON.stringify(detteInnlegget));
+
+    }
+    innleggBoks.innerHTML = nyHTML;
+    if(innlegg.length > 0) {
+	kommentarKnapper();
+    }
+}
+
+document.getElementById("nytt-innlegg").addEventListener("click", function() {
+    var nyttInnlegg = document.getElementById("nytt-innlegg-tekst").value;
+    innlegg.push({
+	"tekst": nyttInnlegg,
+	"kommentarer": []
+    });
+
+    oppdaterInnlegg();
+});
+
+function kommentarKnapper() {
+    document.getElementById("kommentar-send").addEventListener("click", function() {
+	var navn = document.getElementById("kommentar-navn").value;
+	var kommentar = document.getElementById("kommentar-kommentar").value;
+
+	innlegg[innlegg.length - 1].kommentarer.push({
+	    "navn": navn,
+	    "kommentar": kommentar
+	});
+	
+	oppdaterInnlegg();
+    });
+}
