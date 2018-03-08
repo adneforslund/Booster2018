de959115ac1958a8c6ab521d51b318c45a1824f9
Ådne Gutubakken Forslund <adne.forslund@student.uib.no>
2018-03-08 21:49:34 +0100

    waaat

--- util/kidlib.js
+++ util/kidlib.js
@@ -1,3 +1,12 @@
 var hentId = function(id) { return document.getElementById(id); }
 var hentKlasse = function(id) { return document.getElementByClassName(id); }
 var leggTilKnapp = function(id, func) { return document.getElementById(id).addEventListener("click", func); }
+var enterKnapp = function(id, func){ 
+    
+document.getElementById(id).addEventListener("keyup", function (event){
+event.preventDefault();
+if (event.keyCode === 13) 
+    return document
+        .getElementById("myBtn")
+        .click();
+})}
